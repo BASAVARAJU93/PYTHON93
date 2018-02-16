b7259a2a2686eb120bfef84d51a25d7daddc42ca
BASAVARAJU93 <36529954+BASAVARAJU93@users.noreply.github.com>
2018-02-16 14:23:09 +0530

    Update work.py

--- work.py
+++ work.py
@@ -1,4 +1,4 @@
-a=input("enter the value of a:")
-b=input("enter the value of b:")
+a=int(input("enter the value of a:"))
+b=int(input("enter the value of b:"))
 ans=(a*a)+(b*b)+(2*a*b)
 print("The ans is :", ans,"!!")
